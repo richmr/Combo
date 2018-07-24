4ad642cada1061ef77da22764b8b17aa1ff14f9f
Mike <mike@tofet.net>
2018-07-23 22:04:05 -0700

    Charts working
    Charts are plotting
    Combo list generator is working, but GUI not done

---
+++ js/comboList.js
@@ -0,0 +1,102 @@
+/*
+Copyright 2018 Michael R Rich (mike@tofet.net)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Generates a list of possible combinations... Stops at 200 total
+
+function comboListInit() {
+	$("#combo_list").click(function(event) {
+		clickOnComboList();		
+	});
+}
+
+function clickOnGuestList () {
+	// Generate the data in the guest list
+	// Clear the guest list
+	htmlsnippet = "Things go here"	
+	
+	$("#comboList-content").html();
+	
+	// Open the modal
+	$("#comboListModal").modal('open');
+}
+
+function rareReadings(readingsHistogram, level = 1) {
+	// Generates an array of combo readings that meet a rarity level
+	// level 1 = only numbers that meet the lowest number of observations in the histogram
+	// level 2 = numbers that meet the next lowest number of readings in the histogram and the lowest
+	// Anything over that level of rarity probably isn't value added
+	// This isn't my best algorithmic creation...
+	// Need to create a copy of the array, so we don't modify original data!
+	var copyOfReadings = readingsHistogram.slice();
+	var rareReadingsIndex = [];
+	for (run = 0; run < level; run++) {
+		var minVal = Math.min(...copyOfReadings);  //  For the record I've never heard of that "..." operator before.
+		var maxVal = Math.max(...copyOfReadings); // Just wait until you see what I do with this..  Hack time..
+		copyOfReadings.forEach( function (value, index, theCopy) {
+			if (value == minVal) {
+				rareReadingsIndex.push(index);
+				theCopy[index] = (maxVal+1);  // This is to make it non-rare for the next cycle while maintaining the index order
+			}
+		});	
+	}
+	
+	// Now convert index locations to possible values
+	var rareReadings = [];	
+	$.each(rareReadingsIndex, function (index, value) {
+			rareReadings.push(viableEntries[value]);
+		});
+		
+		return rareReadings;
+}
+
+function possibleCombinations(rareReadings, maxCombos=200) {
+	// rareReadings is an array of arrays of rare readings for each dial, where index 0 is the first dial
+	(indexTrack = []).length = rareReadings.length;
+	indexTrack.fill(0);
+	done = false;
+	comboCount = 0;
+	var comboList = [];
+	while (!done) {
+			var aCombo = [];
+			for (i = 0; i < rareReadings.length; i++) {
+				aCombo.push(rareReadings[i][indexTrack[i]]);			
+			}
+			comboList.push(aCombo);
+			comboCount += 1;
+			if (comboCount >= maxCombos) {
+				done = true;			
+			} else {
+				// Increment the sample
+				indexTrack[rareReadings.length-1] += 1;
+				for (j = (rareReadings.length - 1); j >= 0; j--) {
+					if (indexTrack[j] >= rareReadings[j].length) {
+						if ((j-1) < 0) {
+							//  Then we have finished all the combos
+							done = true;
+							break;
+						} else {
+							indexTrack[j] = 0;
+							indexTrack[j-1] += 1;						
+						}					
+					}				
+				}
+			
+			}
+	}
+	return comboList;
+}
--- js/comboStats.js
+++ js/comboStats.js
@@ -16,6 +16,140 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// TODO: Make this editable from the UI to allow for different types of locks
+viableEntries = [0,1,2,3,4,5,6,7,8,9];
+
+// I looked for a way to get the chart object from the canvas reference, but all the answers were using a global variable..  Sigh
+chartRefs = {};
+var dialHistograms = {};
+
 function doStats() {
-	console.log("I've got " + _.size(comboReadings) + " stats to work with");
+	console.log("doStats(): I've got " + _.size(comboReadings) + " stats to work with");
+	/*
+		Overall process here:
+		Break inputs into digits and put into dial arrays
+		Calculate histogram
+		Draw the results
+		Display low seen numbers per dial
+	*/
+	
+	dialReadings = getReadingsForDials();
+	// You can't set a desired bin range in jStat.histogram, it only works on the range present in the data.
+	// Two options: add a "fake" entry for each possibility in the range, and then subtract 1 from each entry in the built histogram
+	// Or do the count manually.
+	// Lets try Plan A
+	$.each(dialReadings, function (dialID, readings) {
+		readings = _.concat(readings, viableEntries);
+		dialHistogram = jStat.histogram(readings, viableEntries.length);
+		// Remove "fake" readings
+		dialHistogram = dialHistogram.map(function (value) {return (value-1);});
+		// Save to global object for combo gen
+		dialHistograms[dialID] = dialHistogram;
+		// Update plots
+		doChart(dialID, dialHistogram);
+	});	  		
+}
+
+function doChart(dialID, dialData) {
+	// Does a ref for this chart exist already?
+	if (chartRefs[dialID]) {
+		// Just update data
+		updateChart(dialID, dialData);			
+	} else {
+		// Make it
+		initDialChart(dialID);
+		updateChart(dialID, dialData);	
+	}
+}
+
+function resetCharts() {
+	// Just removes all the charts
+	$("#dial-stats").html('Dial Results:<span id ="dial-stats-bottom"></span>');
+}
+
+function addChartCanvas(dialID) {
+	// Adds a new chart canvas
+	chartID = "dialChart-"+dialID;
+	htmlsnippet = '<canvas id="'+chartID+'"></canvas>';
+	$("#dial-stats-bottom").before(htmlsnippet);
+	return chartID;
+}
+
+function initDialChart(dialID) {
+	// Add the canvas
+	chartID = addChartCanvas(dialID);
+	// Make the chart
+	var ctx = document.getElementById(chartID).getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: viableEntries,
+	        datasets: [{
+	            label: 'Dial ' + (dialID+1) + ' Stats',
+	            data: [],
+	            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+	            borderColor: 'rgba(54, 162, 235, 1)',
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true,
+	                    stepSize: 1
+	                }
+	            }]
+	        }
+	    }
+	});
+	// Save the ref
+	chartRefs[dialID] = myChart;
+}
+
+function updateChart(dialID, dialData) {
+	myChart = chartRefs[dialID];
+	myChart.data.datasets[0].data = dialData;
+	myChart.update();
+}
+
+function drawChart(dialID, dialData) {
+	var ctx = document.getElementById("dialChart-1").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: viableEntries,
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
+
+function getReadingsForDials() {
+	// Returns an array of arrays of the readings for given dial number
+	var numDials = $("#dialreading-1").val().length;
+	// Make an array of empty arrays
+	var readingArray = [];
+	for (i = 0; i < numDials; i++) {
+		readingArray.push([]);	
+	}
+	$.each(comboReadings, function (key, reading) {
+		for (i = 0; i < reading.length; i++) {
+			readingArray[i].push(Number(reading[i]));
+		}
+	});
+	
+	return readingArray;
 }
